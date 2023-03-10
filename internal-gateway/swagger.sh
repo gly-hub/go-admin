d82eb2dcf0a2680581bc388fdf5eaeda2b146d08
fangao <fangao@staruniongame.com>
2023-03-10 10:05:34 +0800

    feat:初始化仓库

--- internal-gateway/swagger.sh
+++ internal-gateway/swagger.sh
@@ -4,13 +4,13 @@
 declare -A swagMap
 # 由于基础信息生成默认main.go文件，所以需要指定一个接口目录下的一个go文件。任意文件即可
 declare -A fileMap
-swagMap["auth"]="0f9195fe3a9d44dea349f27067b1348b817ea331db259427095860da3150e7cb"
+swagMap["auth"]="27845ae85ebd2974b61478705b575a7d48ad5f538c27c6b99777b15456782d74"
 fileMap["auth"]="auth_controller.go"
 
-swagMap["operation"]="0b50b0b4d03f06bee06cdda91309e4f2d94352dce0dc2ea8d7b3d34d3f52e458"
-fileMap["auth"]="operation_controller.go"
+swagMap["operation"]="db575e62667e710556a92e5b81eaf4c3509102d627eaeaa1105f430b557438b5"
+fileMap["operation"]="operation_controller.go"
 
-url='http://yapi.smart-xwork.cn/'
+url='http://47.96.85.188:3000'
 for key in ${!swagMap[@]}; do
   if [[ $# == 1 && $1 != $key ]]; then
     continue
